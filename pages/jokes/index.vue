78798fc5dcf9b514bc3d25deef336937832139fb
anthony collier <anthony-collier@hotmail.fr>
2021-11-27 14:13:03 +0100

    search bar working

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>jokes :
-
-    <Joke v-for="joke in jokes" :key="joke.id" :joke="joke.joke" />
+    <SearchJokes v-on:search-text="searchText"/>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
   </div>
 
 </template>
 <script>
   import axios from "axios";
   import Joke from "~/components/Joke";
+  import SearchJokes from "~/components/SearchJokes";
 
   export default{
     data(){
@@ -16,6 +17,7 @@
       }
     },
     components:{
+      SearchJokes,
       Joke
     },
     async created(){
@@ -28,9 +30,28 @@
       try{
         const res = await axios.get('https://icanhazdadjoke.com/search', config);
         const data = res.data.results;
+        console.log(data);
         this.jokes = data;
       }catch(err){
         console.log(err);
+      }
+    },
+    methods: {
+      async searchText(text) {
+        const config = {
+          headers: {
+            Accept: "application/json"
+          }
+        };
+        try {
+          const res = await axios.get(
+            `https://icanhazdadjoke.com/search?term=${text}`,
+            config
+          );
+          this.jokes = res.data.results;
+        } catch (err) {
+          console.log(err);
+        }
       }
     },
     head(){
