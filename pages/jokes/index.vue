fcc8c448f2cd07abf303732aa57c93bbbb2ebecd
anthony collier <anthony-collier@hotmail.fr>
2021-11-27 13:38:44 +0100

    Joke API working on /jokes

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>jokes</div>
+  <div>jokes :
+
+    <Joke v-for="joke in jokes" :key="joke.id" :joke="joke.joke" />
+  </div>
+
 </template>
 <script>
+  import axios from "axios";
+  import Joke from "~/components/Joke";
+
   export default{
     data(){
       return{
         jokes : []
+      }
+    },
+    components:{
+      Joke
+    },
+    async created(){
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+
+      try{
+        const res = await axios.get('https://icanhazdadjoke.com/search', config);
+        const data = res.data.results;
+        this.jokes = data;
+      }catch(err){
+        console.log(err);
       }
     },
     head(){
