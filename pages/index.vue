220395645e325c85f27ef4aa662bdb6bb5babb04
anthony collier <anthony-collier@hotmail.fr>
2021-11-27 13:18:55 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Welcome to the best corny dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script>
+  import AppHeader from "~/components/AppHeader";
+  export default {
+    components: {AppHeader},
+    head(){
+      return {
+        title: 'Welcome to dad jokes',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Best place for corny dad jokes'
+          }
+        ]
+      }
+    }
+  };
+</script>
---
+++ pages/jokes/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>jokes</div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        jokes : []
+      }
+    },
+    head(){
+      return {
+        title: 'Dad Jokes',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Best place for corny dad jokes'
+          }
+        ]
+      }
+    }
+  };
+</script>
+
+<style></style>
